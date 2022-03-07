d5f53cbb89e7190c2f980853adb97a0c94acf2d3
byjtew93 <benjamin.lozes@protonmail.com>
2022-03-07 15:28:30 +0100

    Adding types abstraction over 64/32bits architectures

--- include/elf_reader.hpp
+++ include/elf_reader.hpp
@@ -5,9 +5,14 @@
 #ifndef C_BDD_ELF_READER_HPP
 #define C_BDD_ELF_READER_HPP
 
+#include <cstdint>
 #include "elf.h"
 
+#if INTPTR_MAX == INT64_MAX // 64 BITS ARCHITECTURE
+using Elf_Ehdr = Elf64_Ehdr;
+#elif INTPTR_MAX == INT32_MAX // 32 BITS ARCHITECTURE
 using Elf_Ehdr = Elf32_Ehdr;
+#endif
 
 [[nodiscard]] bool isElfFile(Elf_Ehdr &file);
 
