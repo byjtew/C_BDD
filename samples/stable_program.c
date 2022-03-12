2cab49878f8c6bdecafd52f813b47aa0712432ee
byjtew93 <benjamin.lozes@protonmail.com>
2022-03-13 00:36:34 +0100

    Switching the sample program to C (elf function names)
    C++ function names translation will be done with boost, later.

---
+++ samples/stable_program.c
@@ -0,0 +1,29 @@
+//
+// Created by byjtew on 05/03/2022.
+//
+
+#include <stdio.h>
+
+void func() {
+  printf("Printing from a function [0x%016hX]\n", (char *) func);
+}
+
+int main() {
+  printf("Hello, I am a stable tin program.");
+  printf(
+      "I have:\n\t- 1 int variable\n\t- 1 char variable\n\t- 1 unsigned long variable\n\t- 1 int vector\n\t- 1 int list structure\n\t- 1 function call\n");
+
+  int a = 16;
+  printf("int var: %d\n", a);
+
+  char b = 'c';
+  printf("char var: %d\n", b);
+
+  unsigned long ul = 24UL;
+  printf("unsigned long var: %lu\n", ul);
+
+  func();
+
+  printf("Goodbye.\n");
+  return 0;
+}
