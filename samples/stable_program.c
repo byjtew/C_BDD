6ee99b225406ae17e82d6db3b10c64ecfe78cb52
byjtew93 <benjamin.lozes@protonmail.com>
2022-03-15 11:11:14 +0100

    Adding libunwind libraries in CMake
    Source: https://github.com/monero-project/monero/blob/master/cmake/FindLibunwind.cmake

--- samples/stable_program.c
+++ samples/stable_program.c
@@ -3,13 +3,15 @@
 //
 
 #include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
 
 void func() {
-  printf("Printing from a function: [0x%016lX]\n", func);
+  printf("Printing from a function: [0x%016lX]\n", (uintptr_t) func);
 }
 
 int main() {
-  printf("Hello, I am a stable tin program.");
+  printf("Hello, I am a stable program. My PID is %d\n", getpid());
   printf(
       "I have:\n\t- 1 int variable\n\t- 1 char variable\n\t- 1 unsigned long variable\n\t- 1 int vector\n\t- 1 int list structure\n\t- 1 function call\n");
 
@@ -22,6 +24,8 @@ int main() {
   unsigned long ul = 24UL;
   printf("unsigned long var: %lu\n", ul);
 
+  printf("60 seconds nap...\n");
+  sleep(60);
   func();
 
   printf("Goodbye.\n");
